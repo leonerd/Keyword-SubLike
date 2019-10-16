666643628e4e1e5354bab2651d7d32cbe6b79e9c
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2019-10-16 15:17:52 +0100

    Initial empty skeleton

---
+++ lib/Keyword/SubLike.pm
@@ -0,0 +1,13 @@
+#  You may distribute under the terms of either the GNU General Public License
+#  or the Artistic License (the same terms as Perl itself)
+#
+#  (C) Paul Evans, 2019 -- leonerd@leonerd.org.uk
+
+package Keyword::SubLike;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+0x55AA;
